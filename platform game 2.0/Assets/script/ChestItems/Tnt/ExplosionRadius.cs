ecffb044665cc106de8799edc6084b236e83ce89
Jamydewaalmedia <35739@ma-web.nl>
2024-03-17 13:11:52 +0100

    changed the explosionradius from on trigger enter to on collision so you cant go through it if its not broken

--- platform game 2.0/Assets/script/ChestItems/Tnt/ExplosionRadius.cs
+++ platform game 2.0/Assets/script/ChestItems/Tnt/ExplosionRadius.cs
@@ -5,13 +5,13 @@ using UnityEngine.Animations;
 public class ExplosionRadius : MonoBehaviour
 {
     [SerializeField] private string tag = "breakable"; 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         // Check if the collided object has the "breakable" tag
-        if (other.CompareTag(tag))
+        if (collision.gameObject.CompareTag(tag))
         {
             // Destroy the collided object
-            Destroy(other.gameObject);
+            Destroy(collision.gameObject);
             GameObject parentObject = transform.parent.gameObject;
         
             // Destroy the parent GameObject
