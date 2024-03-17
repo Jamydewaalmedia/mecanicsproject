1cb56e50c50d39477c0d4c24fab9ccd9f219070f
Jamydewaalmedia <35739@ma-web.nl>
2024-03-17 13:01:46 +0100

    added customizable x and y for spawning

--- platform game 2.0/Assets/script/Instantiate.cs
+++ platform game 2.0/Assets/script/Instantiate.cs
@@ -5,7 +5,8 @@ public class InstantiateObject : MonoBehaviour
 {
     public GameObject prefabToInstantiate; // Het prefab om te instantiëren
     private bool playerCollided = false; // Een variabele om bij te houden of de speler al heeft gebotst
-
+    [SerializeField] private float spawnY = 0;
+    [SerializeField] private float spawnX = 0;
     void Update()
     {
        
@@ -27,9 +28,9 @@ public class InstantiateObject : MonoBehaviour
                 Debug.Log("Prefab om te instantiëren is niet toegewezen in de inspector!");
                 return;
             }
-
+            Vector3 spawnPosition = transform.position + new Vector3(spawnX, spawnY, 0);
             // Instantieer het prefab op dezelfde positie als dit GameObject
-            Instantiate(prefabToInstantiate, transform.position, transform.rotation);
+            Instantiate(prefabToInstantiate, spawnPosition , transform.rotation);
 
             // Vernietig dit GameObject
             Destroy(gameObject);
